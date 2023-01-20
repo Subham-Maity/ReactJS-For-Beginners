176f7fa7a6f893f4ec248a5ff4b37e5b112cc697
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-01-20 06:42:58 +0530

    Add Image 2 In JSX

--- React 2023 (Better)/04. JSX/jsx-explain/src/App.js
+++ React 2023 (Better)/04. JSX/jsx-explain/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+let name = "Subham"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <div className="container">
+          <h1>Hello {name}</h1>
+         </div>
+      </>
   );
 }
 
