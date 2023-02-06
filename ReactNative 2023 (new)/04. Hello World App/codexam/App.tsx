3319ad144ff3a5b31db73ba8ec791ee84e0f816d
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-02-06 21:59:35 +0530

    Add hello world app

---
+++ ReactNative 2023 (new)/04. Hello World App/codexam/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text> Hello World !</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+export default App;
