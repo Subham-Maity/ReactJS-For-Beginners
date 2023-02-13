98d2020b5ca724d564692f03309f510476c84ff8
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-02-13 21:49:06 +0530

    Hello World App Again

--- ReactNative 2023 (new)/ExperimentProject-1/CodeXam/AppPro.tsx
+++ ReactNative 2023 (new)/ExperimentProject-1/CodeXam/AppPro.tsx
@@ -1,51 +1,43 @@
-import React, { useState } from 'react'
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+import{
+    View,
+    Text,
+    StyleSheet,
+    useColorScheme,
+} from 'react-native'
 
 function AppPro():JSX.Element{
 
-    const [mode, setMode] = useState<'light' | 'dark'>('light')
-    const [bgColor, setBgColor] = useState('#fff')
-
-    const switchMode = () => {
-        if (mode === 'light') {
-            setMode('dark')
-            setBgColor('#333')
-        } else {
-            setMode('light')
-            setBgColor('#fff')
-        }
-    }
+    const isDarkMode = useColorScheme() === 'dark'
+    
 
     return(
-        <View style={[styles.container, { backgroundColor: bgColor }]}>
-            <Text style={mode === 'light' ? styles.blackText : styles.whiteText}>
+
+        <View style={styles.container}>
+
+            <Text style={isDarkMode ? styles.whiteText: styles.blackText }>
                 Hello World!
+                
             </Text>
-            <TouchableOpacity onPress={switchMode} style={styles.button}>
-                <Text style={mode === 'light' ? styles.blackText : styles.whiteText}>
-                    Switch Mode
-                </Text>
-            </TouchableOpacity>
         </View>
     )
+  
+ 
 }
 
 const styles = StyleSheet.create({
+
     container:{
         flex:1,
         alignItems:'center',
-        justifyContent:'center'
+        justifyContent:'center',
     },
     whiteText:{
         color:'#FFFFFF'
     },
     blackText:{
         color:'#000000'
-    },
-    button: {
-        padding: 10,
-        backgroundColor: '#ccc',
-        marginTop: 20
     }
 })
 
