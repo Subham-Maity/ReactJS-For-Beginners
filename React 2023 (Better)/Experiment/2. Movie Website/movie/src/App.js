4ad71b4c76cd02a3bc39ccfde7b98d728a034d43
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-02-20 08:32:05 +0530

    Style Change -> Make it more professional

--- React 2023 (Better)/Experiment/2. Movie Website/movie/src/App.js
+++ React 2023 (Better)/Experiment/2. Movie Website/movie/src/App.js
@@ -1,32 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
-import Movie from './components/Movie';
-import movies from './movie.json'
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Movie from "./components/Movie";
+import movies from "./movie.json";
 
 function App() {
   return (
     <div className="App">
-      <header className="header">
-        <h1>Movie Website</h1>
-      </header>
-      <main className="main">
-        {movies.map((element) => (
-            <Movie 
-            title={element.Title} 
-            year={element.Year} 
-            img={element.Poster} 
+      <Header />
+      <div className="main">
+        {movies.map((element) => {
+          return (
+            <Movie
+              title={element.Title}
+              year={element.Year}
+              img={element.Poster}
             />
-        ))}
-      </main>
-      <footer className="footer">
-        <p>© 2023 Movie Website. All rights reserved.</p>
-      </footer>
+          );
+        })}
+      </div>
     </div>
   );
 }
 
 export default App;
-
-
-
