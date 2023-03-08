dcfa503e1479d3494dffc762a9e02455cfaf3090
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-03-08 21:17:40 +0530

    add

---
+++ ReactNative 2023 (new)/00. Projects/2. Basic Template App/blogApp/components/FlatCards.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+ 
+export default function FlatCards() {
+  return (
+    <View>
+      <Text>FlatCards</Text>
+    </View>
+  )
+}
+ 
+const styles = StyleSheet.create({})
